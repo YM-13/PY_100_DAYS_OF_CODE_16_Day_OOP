4ff413eae48b231900579785f83b61a415ac55c4
YM <exitospec1@gmail.com>
2022-02-18 21:23:09 +0200

    PY_100_D_16_06

--- main.py
+++ main.py
@@ -4,25 +4,23 @@ from money_machine import MoneyMachine
 coffee_money = MoneyMachine()
 process_coffee = CoffeeMaker()
 drinks_order = Menu()
+drink_menu = drinks_order.get_items()
 
-order_name = input(f"What would you like ({drinks_order.get_items()})?: ")
-menu_item_object = drinks_order.find_drink(order_name)
-"""Получаем объект со всеми данными по напитку"""
-if order_name == "report":
-	process_coffee.report()
-	coffee_money.report()
+machine_is_on = True
+while machine_is_on:
+	order_name = input(f"What would you like ({drink_menu})?: ")
+
+	"""Получаем объект со всеми данными по напитку"""
 
 
-else:
-	if process_coffee.is_resource_sufficient(drinks_order.find_drink(order_name)) != None:
-		#coffee_money.make_payment(MenuItem(drinks_order.find_drink(order_name)))
-		print(drinks_order.find_drink(order_name))
-		process_coffee.make_coffee(drinks_order.find_drink(order_name))
+	if order_name == "report":
+		process_coffee.report()
+		coffee_money.report()
+	elif order_name == "off":
+		machine_is_on = False
 	else:
-		print("Impossible to make")
-
-
-
-
-
+		menu_item_object = drinks_order.find_drink(order_name)
+		if process_coffee.is_resource_sufficient(menu_item_object) and coffee_money.make_payment(menu_item_object.cost):
+			# price = menu_item_object.cost  Получает цену из объекта !!!!!!!
+			process_coffee.make_coffee(menu_item_object)
 
